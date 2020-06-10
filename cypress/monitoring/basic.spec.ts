45f60da3f84a68df69ed2eabb7ace6beaa6e96ef
Cezar <cezar.guimaraes@vtex.com.br>
2020-06-10 14:46:56 -0300

    Add monitoring test

---
+++ cypress/monitoring/basic.spec.ts
@@ -0,0 +1,31 @@
+describe('Basic test', () => {
+  before(() => {
+    cy.setVtexIdCookie()
+  })
+
+  it('adds custom bell to cart', () => {
+    // navigate to home page
+    cy.visit('/')
+
+    // search for "bell"
+    cy.get('#downshift-0-input')
+      .type('bell{enter}')
+
+    // wait until search results are loaded
+    cy.get('#search-result-anchor', { timeout: 10000 })
+
+    // find Custom Bell product summary
+    cy.get('section.vtex-product-summary-2-x-container')
+      .contains('Custom Bell')
+      .within(() => {
+        // click Add to cart button
+        cy.get('button')
+          .contains('Add to cart')
+          .click()
+      })
+
+    // assert that the product was added to the cart
+    cy.get('.vtex-toast')
+      .should('contain.text', "Item added to your cart")
+  })
+})
